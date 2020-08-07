d7ff4e93474c6b15b04946113532c3ab969b130f
MinhTung2504 <54889067+MinhTung2504@users.noreply.github.com>
2020-08-07 22:30:49 +0700

    Add files via upload

---
+++ javatictactoe.java
@@ -0,0 +1,10 @@
+package minhtung.demo;
+
+public class javatictactoe {
+
+	public static void main(String[] args) {
+		Java_tic_tac_toe game  = new Java_tic_tac_toe("Game tic tac toe");
+		game.setVisible(true);
+	}
+
+}
